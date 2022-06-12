8f66ddfb9d768ed00d31481255f252e9c474d681
KryKrys <krymel24@gmail.com>
2022-06-11 21:44:01 -0600

    casa

--- caperucita.c
+++ caperucita.c
@@ -17,22 +17,34 @@ caperucita[24]={
     51,352
 };
 
-casa[26]={
-    449,0,
-    414,73,
-    421,84,
-    492,84,
-    491,407,
-    640,464,
-    640,65,
-    495,65,
-    540,0,
-    466,0,
-    495,65,
-    493,91,
-    504,97,
-    640,100
-}
+casa[22]={
+    489,0,
+    422,85,
+    424,105,
+    508,0,
+    571,0,
+    487,103,
+    640,80,
+    640,470,
+    486,390,
+    487,103,
+    424,105
+};
+
+puerta[22]={
+    524,410,
+    524,213,
+    534,198,
+    547,187,
+    560,181,
+    570,182,
+    584,190,
+    593,200,
+    603,215,
+    609,231,
+    608,455
+};
+
 
 void fondo(void);
 
@@ -40,14 +52,17 @@ void fondo(void);
 
 void main(){
 
-    int modo, driver = DETECT;
+	int modo, driver = DETECT;
     int x;
 
     initgraph(&driver, &modo, "");
 
     fondo();
-    drawpoly(13,casa);
+    
     while(kbhit()==0){
+        setfillstyle(SOLID_FILL,LIGHTBLUE);
+        bar(0,0,640,350);
+        
         setfillstyle(SOLID_FILL,WHITE);
         bar(100+x,20,150+x,40);
         bar(10+x,120,60+x,140);
@@ -57,16 +72,21 @@ void main(){
         setfillstyle(SOLID_FILL,YELLOW);
         floodfill(0,0,WHITE);
 
+        
+
+        
+
         if(x<640){
             x+=10;
         }else{
             x=10;
         
         }
-
+        
+        drawpoly(11,casa);
+        drawpoly(11,puerta);
         delay(300);
-        setfillstyle(SOLID_FILL,LIGHTBLUE);
-        bar(0,0,640,350);
+
         
     }
 
@@ -77,4 +97,4 @@ void fondo(){
     bar(0,0,640,480);
     setfillstyle(SOLID_FILL,LIGHTGREEN);
     bar(0,350,640,480);
-}
+}
