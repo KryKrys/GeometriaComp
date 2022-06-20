8a0c0211f1d841f4c34d4005f1e07e8cb4c14fa3
KryKrys <krymel24@gmail.com>
2022-06-19 19:54:59 -0600

    principal

--- hanoi.c
+++ hanoi.c
@@ -1,13 +1,28 @@
 #include <graphics.h>
 #include <stdio.h>
+#include <conio.h>
+#include <button.h>
+
+void princip(void);
 
 void main(){
 
     int modo, driver = DETECT;
 
+   
+
     initgraph(&driver, &modo, "");
 
     cleardevice();
+    
+    princip();
+
+    getch();
+
+}
+
+void princip(){
+    button jugar, inst, salir;
 
     setfillstyle(SOLID_FILL,YELLOW);
     bar(0,0,640,480);
@@ -19,6 +34,11 @@ void main(){
     outtextxy(170,70,"Torres de ");
     outtextxy(240,130,"Hanoi");
 
-    getch();
+    newButton(&jugar, 250,320, "jugar",RED,BLACK,140,30);
+    newButton(&inst, 220,350, "instrucciones",CYAN,BLACK,210,30);
+    newButton(&salir, 190,380, "salir",GREEN,BLACK,260,30);
 
+    setfillstyle(SOLID_FILL,BROWN);
+    bar(160,410,480,430);
+    bar(310,269,330,319);
 }
