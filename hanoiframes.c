66c9f9ac1d747263332961cccf984837bc791e81
Manorag <guillermoernestocastillo@outlook.com>
2022-06-28 18:02:52 -0600

    Amago del juego

--- hanoiframes.c
+++ hanoiframes.c
@@ -2,49 +2,195 @@
 #include <graphics.h>
 #include <stdio.h>
 #include<stdlib.h>
-#include<BMP256.h>
-#include<256IMAGE.h>
+
+
+#define arriba 72
+#define abajo 80
+#define izquierda 75
+#define derecha 77
+#define punto 46
+#define coma 44
+#define comi 39
+#define ESC 27
+
+char tecla;
+
  
 /*Las funciones p# son para mostrar los diferentes frames, en cada una de ellas se dibujara un movimiento*/
 void p7()
 {
+     cleardevice();
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(440, 346, 550, 373);
+    floodfill(448, 349, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(460, 315, 535, 344);
+    /*ancho 30 largo 75*/
+    floodfill(497, 318, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(480, 288, 510, 310);
+    floodfill(487, 290, 15);
+ 
+    
+    outline();
+    delay(1000);
   
 }
  
 
 void p6()
 {
+      cleardevice();
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(440, 346, 550, 373);
+    floodfill(448, 349, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(460, 315, 535, 344);
+    /*ancho 30 largo 75*/
+    floodfill(497, 318, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(135, 356, 165, 376);
+    floodfill(146, 357, 15);
+ 
+    
+    outline();
+    delay(1000);
    
 }
  
 
 void p5()
 {
+    cleardevice();
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(440, 346, 550, 373);
+    floodfill(448, 349, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(290, 345, 350, 375);
+    /*ancho 30 largo 75*/
+    floodfill(297, 360, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(135, 356, 165, 376);
+    floodfill(146, 357, 15);
+ 
+    
+    outline();
+    delay(1000);   
         
 }
  
 
 void p4()
 {
+    cleardevice();
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(440, 346, 550, 373);
+    floodfill(448, 349, 15);
+ 
    
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(290, 345, 350, 375);
+    /*ancho 30 largo 75*/
+    floodfill(297, 360, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(305, 326, 335, 345);
+    floodfill(316, 337, 15);
+ 
+    
+    outline(); 
+    delay(1000);
 }
+   
+
  
 
 void p3()
 {
+    cleardevice();
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(94, 346, 204, 373);
+    floodfill(98, 349, 15);
+ 
    
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(290, 345, 350, 375);
+    /*ancho 30 largo 75*/
+    floodfill(297, 360, 15);
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(305, 326, 335, 345);
+    floodfill(316, 337, 15);
+ 
+    
+ 
+    outline();
+    delay(1000);
+
 }
  
 
 void p2()
 {
 
+    cleardevice();
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(94, 346, 204, 373);
+    floodfill(98, 349, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(290, 345, 350, 375);
+    /*ancho 30 largo 75*/
+    floodfill(297, 360, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(480, 356, 510, 376);
+    floodfill(486, 357, 15);
+ 
+    
+    outline();
+    delay(1000);
+
 }
  
 
 void p1()
 {
+    cleardevice();
+    
+    
+    setfillstyle(SOLID_FILL, RED);
+    rectangle(94, 346, 204, 373);
+    floodfill(98, 349, 15);
+ 
    
+    setfillstyle(SOLID_FILL, BLUE);
+    rectangle(111, 314, 186, 342);
+    floodfill(113, 320, 15);
+ 
+   
+    setfillstyle(SOLID_FILL, GREEN);
+    rectangle(480, 356, 510, 376);
+    floodfill(486, 357, 15);
+ 
+    
+    outline();
+    delay(1000);
 }
  
 
@@ -103,29 +249,49 @@ void main()
     int modo, driver = DETECT;
     initgraph(&driver, &modo, "");
     cleardevice();
-   
+
     start();
- 
-   
-    p1();
- 
-   
-    p2();
- 
-   
-    p3();
- 
-   
-    p4();
- 
-  
-    p5();
- 
-   
-    p6();
- 
     
-    p7();
+  
+  do{
+    if(kbhit()){
+			tecla = getch();
+			switch(tecla){
+        
+    case arriba:
+        p1();
+        break;
+
+    case abajo:
+        p2();
+        break;
+ 
+    case izquierda:
+        p3();
+        break;
+ 
+    case derecha:
+        p4();
+        break;
+    
+    case punto:
+        p5();
+        break;
+
+    case coma:
+        p6();
+        break;
+ 
+    case comi:
+        p7();
+        break;
+    
+    case ESC:
+        break;
+    }
+        }
+    
+    }while(tecla!=ESC);
  
     
     getch();
